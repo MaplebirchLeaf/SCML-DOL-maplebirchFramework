e790f8e062bd2770667d629bf65f36fb3c5c5d7e
MaplebirchLeaf <186996260+MaplebirchLeaf@users.noreply.github.com>
2026-02-14 17:30:19 +0800

    Merge 'BUN' and subsequent changes

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,19 +1,12 @@
 import path from 'path';
 import { rspack } from '@rspack/core';
 import type { Configuration } from '@rspack/core';
-<<<<<<< HEAD
 import { production } from './scripts/production';
-=======
->>>>>>> 11f091a6af963002e33cded9527680448f073973
 
 export default (env: unknown, argv: { mode?: string }): Configuration => {
   const isProduction = argv.mode === 'production';
 
-<<<<<<< HEAD
   const config: Configuration = {
-=======
-  return {
->>>>>>> 11f091a6af963002e33cded9527680448f073973
     entry: './src/main.ts',
     output: {
       path: path.resolve(__dirname, 'dist'),
@@ -54,37 +47,8 @@ export default (env: unknown, argv: { mode?: string }): Configuration => {
         },
       ],
     },
-<<<<<<< HEAD
     performance: {
       hints: false,
-=======
-    optimization: isProduction
-      ? {
-          minimize: true,
-          minimizer: [
-            new rspack.SwcJsMinimizerRspackPlugin({
-              minimizerOptions: {
-                compress: {
-                  passes: 3,
-                },
-                format: {
-                  comments: false,
-                },
-                mangle: true,
-                ecma: 2022,
-              },
-            }),
-          ],
-          usedExports: true,
-          sideEffects: true,
-          concatenateModules: true,
-        }
-      : {},
-    performance: {
-      hints: false,
-      maxEntrypointSize: 2 * 1024 * 1024,
-      maxAssetSize: 2 * 1024 * 1024,
->>>>>>> 11f091a6af963002e33cded9527680448f073973
     },
     plugins: [
       new rspack.CopyRspackPlugin({
@@ -92,7 +56,6 @@ export default (env: unknown, argv: { mode?: string }): Configuration => {
       }),
     ],
   };
-<<<<<<< HEAD
 
   if (isProduction) {
     return {
@@ -103,6 +66,3 @@ export default (env: unknown, argv: { mode?: string }): Configuration => {
 
   return config;
 };
-=======
-};
->>>>>>> 11f091a6af963002e33cded9527680448f073973
