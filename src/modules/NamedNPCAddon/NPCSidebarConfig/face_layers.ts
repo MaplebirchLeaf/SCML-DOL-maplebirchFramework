323af90827b15cbb9847a71168fd4d8781595858
MaplebirchLeaf <186996260+MaplebirchLeaf@users.noreply.github.com>
2026-03-04 09:40:36 +0800

    fix Layer Zindex

--- src/modules/NamedNPCAddon/NPCSidebarConfig/face_layers.ts
+++ src/modules/NamedNPCAddon/NPCSidebarConfig/face_layers.ts
@@ -8,13 +8,13 @@ const face_layers = {
   nnpc_face_main: clothes_layer('face', 'main', {
     zfn(options: LayerOptions) {
       if (options.maplebirch.nnpc.clothes.face.type.includes('glasses')) return maplebirch.char.ZIndices.over_head + options.maplebirch.nnpc.position!;
-      return maplebirch.char.ZIndices.face + options.maplebirch.nnpc.position!;
+      return maplebirch.char.ZIndices.facewear + options.maplebirch.nnpc.position!;
     }
   }),
   nnpc_face_acc: clothes_layer('face', 'acc', {
     zfn(options: LayerOptions) {
       if (options.maplebirch.nnpc.clothes.face.type.includes('glasses')) return maplebirch.char.ZIndices.over_head + options.maplebirch.nnpc.position!;
-      return maplebirch.char.ZIndices.face + options.maplebirch.nnpc.position!;
+      return maplebirch.char.ZIndices.facewear + options.maplebirch.nnpc.position!;
     }
   }),
   nnpc_face_back: clothes_back('face'),
