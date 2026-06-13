847369977aff3873bf3d2a6a964a2b90bb0d2312
MaplebirchLeaf <186996260+MaplebirchLeaf@users.noreply.github.com>
2026-06-13 18:18:50 +0800

    update version-4.1.4

--- bump.config.js
+++ bump.config.js
@@ -1,6 +1,6 @@
 export default {
   files: ['package.json', 'packages/types/package.json'],
   tag: 'maplebirch-release-v%s',
-  commit: 'chore(release): v%s',
+  commit: 'maplebirch-release-v%s',
   push: false
 };
